6bb882517465408ac93cdba71f4a1c66634ce4a7
Dmytro Barsukov <dbarsukov@nymbus.com>
2025-04-17 03:27:21 +0300

    featuer: add ComponentTuple typedef

--- game1/src/typedefs.h
+++ game1/src/typedefs.h
@@ -1,7 +1,15 @@
 #pragma once
+
 #include <cstdint>
+#include <tuple>
+
+#include "components.hpp"
 
 const int HEIGHT = 700;
 const int WIDTH = 1280;
 
 typedef uint32_t Entity;
+using ComponentTuple = std::tuple<
+	Position,
+	Velocity
+>;
