96abd40fe183ca4c106345d1ad2b977e36f58d6b
Dmytro Barsukov <dbarsukov@nymbus.com>
2025-04-21 23:26:49 +0300

    feat: add EnittyVec and EntityMap typedefs

--- game1/src/typedefs.h
+++ game1/src/typedefs.h
@@ -2,11 +2,15 @@
 
 #include <cstdint>
 #include <tuple>
+#include <unordered_map>
 
 #include "Components.hpp"
 
 const int HEIGHT = 700;
 const int WIDTH = 1280;
+
+using EntityVec = std::vector<std::shared_ptr<Entity>>;
+using EntityMap = std::unordered_map<std::string, EntityVec>;
 
 using ComponentTuple = std::tuple<
 	CPosition,
