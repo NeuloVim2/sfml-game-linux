58815e4facec302bf23b572d7c73d50173ef3c56
Dmytro Barsukov <dbarsukov@nymbus.com>
2025-04-22 18:24:11 +0300

    feat: add member variables for EntityManager

--- game1/src/EntityManager.h
+++ game1/src/EntityManager.h
@@ -3,11 +3,14 @@
 #define ENTITY_MANGER_H
 
 #include "Entity.h"
+#include "typedefs.h"
 
 class EntityManager
 { 
 private:
-
+	EntityVec m_entities;
+	EntityVec m_enityMap;
+	uint8_t m_totalentities = 0;
 public:
 	EntityManager() {};
 };
