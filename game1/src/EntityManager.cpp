b427d777c1e15e199aeec729d8b57cbb13782c37
Dmytro Barsukov <dbarsukov@nymbus.com>
2025-04-24 01:32:23 +0300

    feat: update addEntity() to avoid iterator invalidation

--- game1/src/EntityManager.cpp
+++ game1/src/EntityManager.cpp
@@ -26,7 +26,6 @@ void EntityManager::update()
 std::shared_ptr<Entity> EntityManager::addEntity(const std::string& tag) 
 {
 	auto ent = std::make_shared<Entity>(tag, m_totalentities++);
-	m_entities.push_back(ent);
-	m_enityMap[tag].push_back(ent);
+	m_toAdd.push_back(ent);
 	return ent;
 }
--- game1/src/EntityManager.h
+++ game1/src/EntityManager.h
@@ -14,11 +14,12 @@ class EntityManager
 { 
 private:
 	EntityVec m_entities;
+	EntityVec m_toAdd;
 	EntityMap m_enityMap;
 	uint8_t m_totalentities = 0;
 public:
 	EntityManager() {};
-
+	void update();
 	std::shared_ptr<Entity> addEntity(const std::string& tag);
 };
 
